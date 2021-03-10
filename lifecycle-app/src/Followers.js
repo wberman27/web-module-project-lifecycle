b5296e331fe0e481a0fb662c1db60c534a748def
Will B <wberman27@gmail.com>
2021-03-09 21:56:33 -0500

    Followers component is functional

--- lifecycle-app/src/Followers.js
+++ lifecycle-app/src/Followers.js
@@ -6,12 +6,9 @@ export default function Followers(props) {
     return(
         <>
         <div className = 'followersCard'>
-            <h3>Followers: </h3>
-            <img width='200px' src={props.follower.avatar_url} alt='github Followers profile avatar'/>
-            {/* <h2>{props.follower.name}</h2>
-            <p>{props.follower.bio}</p>
-            <p>Location: {props.follower.location}</p>
-            <a href={props.follower.html_url} target='_blank'>Go to {props.follower.name}'s GitHub Page</a> */}
+            <h2>{props.follower.login}</h2>
+            <img className = 'followerImg' width='200px' src={props.follower.avatar_url} alt='github Followers profile avatar'/>
+            <a href={props.follower.html_url} target='_blank'>{props.follower.login}'s GitHub Page</a>
 
         </div>
         </>
--- lifecycle-app/src/User.js
+++ lifecycle-app/src/User.js
@@ -7,7 +7,7 @@ export default function User(props) {
     return(
         <>
         <div className = 'userCard'>
-            <img width='200px' src={props.gitHubData.avatar_url} alt='github user profile avatar'/>
+            <img className = 'profileImg' width='200px' src={props.gitHubData.avatar_url} alt='github user profile avatar'/>
             <h2>{props.gitHubData.name}</h2>
             <p>{props.gitHubData.bio}</p>
             <p>Location: {props.gitHubData.location}</p>
@@ -15,7 +15,7 @@ export default function User(props) {
 
         </div>
         <div className = 'followersContainer'>
-            <h3>Followers: </h3>
+            <h3>Followers</h3>
             {props.gitHubFollowers.map(follower => (
                 <Followers follower = {follower}/>
             ))}   
